9a2d410eb3bf6ddd75962134bfd7e3d58cbd9337
Harsh Jaiswal <hsjaiswal3110@gmail.com>
2021-12-19 13:31:54 +0530

    updated

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,2 +1,10 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost/registrationForm",{useNewUrlParser:true},{useUnifiedTopology:true},{useCreateIndex: true},{useFindAndModify: false}).then(()=>console.log("connection successful....")).catch((err)=>console.log(err));
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/registrationForm',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then(()=>console.log("connection successful....")).catch((err)=>console.log(err));
